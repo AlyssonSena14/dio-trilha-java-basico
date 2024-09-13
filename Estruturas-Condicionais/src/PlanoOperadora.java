0682debd2899d545b4717013d1b49a392263b20a
alyssonsena <alyssonsena566@gmail.com>
2024-09-13 20:03:27 -0300

    Estruturas-DE-REPETIÇAO

---
+++ Estruturas-Condicionais/src/PlanoOperadora.java
@@ -0,0 +1,25 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "M";
+
+        if (plano == "B"){
+        System.out.println("100 Minutos de ligaçaõ");
+       }
+       else if (plano == "M"){
+        System.out.println("100 Minutos de Ligação");
+        System.out.println("Whats e instagram grátis ");
+       }
+       else if (plano == "T"){
+        System.out.println("100 Minutos de Ligação");
+        System.out.println("Whats e instagram grátis ");
+        System.out.println("5Gb Youtube");
+        
+       }
+
+
+
+    }
+
+
+    
+}
---
+++ Estruturas-Condicionais/src/SistemadeMedida.java
@@ -0,0 +1,20 @@
+public class SistemadeMedida {
+    public static void main(String[] args) {
+        String sigla = "M";
+        
+        if (sigla == "P"){
+            System.out.println("Pequeno");
+        }
+        else if (sigla == "M") {
+            System.out.println("Medio");
+         }
+         else if (sigla == "G"){
+            System.out.println("Grande");
+         }
+         else {
+            System.out.println("Indefinido");
+         }
+        
+    }
+    
+}
