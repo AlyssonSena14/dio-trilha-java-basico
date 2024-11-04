9158f99cb6705d575aff1d8ff6c226392caa9e34
alyssonsena <alyssonsena566@gmail.com>
2024-11-03 23:50:30 -0300

    feat: Desafio de codigo concluido, e inciando curso de maven.

---
+++ Desafios-De-Codigos/Desafios/VerificandoVelocidadeMediaInternet.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class VerificandoVelocidadeMediaInternet {
+   private double velocidade;
+
+    public double getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(double velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public VerificandoVelocidadeMediaInternet(double velocidade) {
+        this.velocidade = velocidade;
+    }
+}
+class CalculadoraVelocidadeMedia{
+    public  static double calcularVelocidade;
+
+
+
+}
+
+
+
+
+
+
+
+
+
